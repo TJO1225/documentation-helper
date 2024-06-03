f99c023ce2ff546a0bd6f9ff5bd8bf619644ec9d
tjo12 <tjo1225@gmail.com>
2024-06-02 21:32:53 -0700

    Got streamlit to run

--- backend/core.py
+++ backend/core.py
@@ -1,17 +1,4 @@
 from dotenv import load_dotenv
-<<<<<<< HEAD
-=======
-
-load_dotenv()
-
-from langchain_openai import ChatOpenAI, OpenAIEmbeddings
-
-import os
-from typing import Any, Dict, List
-from langchain.chains import ConversationalRetrievalChain
-from langchain_community.vectorstores.pinecone import Pinecone as PineconeLangChain
-from pinecone import Pinecone
->>>>>>> 41d2a9b09c7260fe6a37cc9d5eb62bdf15cc118a
 
 load_dotenv()
 
@@ -21,25 +8,14 @@ from typing import Any, Dict, List
 from langchain.chains.conversational_retrieval.base import ConversationalRetrievalChain
 from langchain_pinecone import PineconeVectorStore
 
-<<<<<<< HEAD
-=======
-pc = Pinecone(api_key=os.environ["PINECONE_API_KEY"])
->>>>>>> 41d2a9b09c7260fe6a37cc9d5eb62bdf15cc118a
 
 INDEX_NAME = "langchain-doc-index"
 
 
 def run_llm(query: str, chat_history: List[Dict[str, Any]] = []):
     embeddings = OpenAIEmbeddings(model="text-embedding-3-small")
-<<<<<<< HEAD
     docsearch = PineconeVectorStore(embedding=embeddings, index_name=INDEX_NAME)
 
-=======
-    docsearch = PineconeLangChain.from_existing_index(
-        embedding=embeddings,
-        index_name=INDEX_NAME,
-    )
->>>>>>> 41d2a9b09c7260fe6a37cc9d5eb62bdf15cc118a
     chat = ChatOpenAI(
         verbose=True,
         temperature=0,
@@ -49,10 +25,7 @@ def run_llm(query: str, chat_history: List[Dict[str, Any]] = []):
         llm=chat, retriever=docsearch.as_retriever(), return_source_documents=True
     )
     return qa.invoke({"question": query, "chat_history": chat_history})
-<<<<<<< HEAD
 
 
 if __name__ == "__main__":
     print(run_llm(query="What is Langchain?"))
-=======
->>>>>>> 41d2a9b09c7260fe6a37cc9d5eb62bdf15cc118a
--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from typing import Set
 from backend.core import run_llm
 import streamlit as st
 from streamlit_chat import message
-<<<<<<< HEAD
 
 
 def create_sources_string(source_urls: Set[str]) -> str:
@@ -18,6 +17,7 @@ def create_sources_string(source_urls: Set[str]) -> str:
 
 
 st.header("LangChain🦜🔗 Udemy Course- Helper Bot")
+
 if (
     "chat_answers_history" not in st.session_state
     and "user_prompt_history" not in st.session_state
@@ -49,57 +49,13 @@ if prompt:
         st.session_state.user_prompt_history.append(prompt)
         st.session_state.chat_answers_history.append(formatted_response)
 
-=======
-
-st.header("LangChain Udemy Course- Documentation Helper Bot")
-
-
-prompt = st.text_input("Prompt", placeholder="Enter your prompt here..")
-
-if "user_prompt_history" not in st.session_state:
-    st.session_state["user_prompt_history"] = []
-
-if "chat_answers_history" not in st.session_state:
-    st.session_state["chat_answers_history"] = []
-
-
-def create_sources_string(source_urls: Set[str]) -> str:
-    if not source_urls:
-        return ""
-    sources_list = list(source_urls)
-    sources_list.sort()
-    sources_string = "sources:\n"
-    for i, source in enumerate(sources_list):
-        sources_string += f"{i+1}. {source}\n"
-    return sources_string
-
-
-if prompt:
-    with st.spinner("Generating response.."):
-        generated_response = run_llm(query=prompt)
-        sources = set(
-            [doc.metadata["source"] for doc in generated_response["source_documents"]]
-        )
-
-        formatted_response = (
-            f"{generated_response['result']} \n\n {create_sources_string(sources)}"
-        )
-
-        st.session_state["user_prompt_history"].append(prompt)
-        st.session_state["chat_answers_history"].append(formatted_response)
-
->>>>>>> 41d2a9b09c7260fe6a37cc9d5eb62bdf15cc118a
 if st.session_state["chat_answers_history"]:
     for generated_response, user_query in zip(
         st.session_state["chat_answers_history"],
         st.session_state["user_prompt_history"],
     ):
-<<<<<<< HEAD
         message(
             user_query,
             is_user=True,
         )
-=======
-        message(user_query, is_user=True)
->>>>>>> 41d2a9b09c7260fe6a37cc9d5eb62bdf15cc118a
         message(generated_response)
